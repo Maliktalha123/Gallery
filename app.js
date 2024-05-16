03a169996782f3daa786066d0f6751efb4cd2bfa
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-16 23:49:43 +0500

    This is a Gallery

---
+++ app.js
@@ -0,0 +1,19 @@
+let listImages = document.getElementsByClassName('listImages')
+console.log(listImages)
+let previewImage = document.getElementById('previewImage')
+
+
+for (let i =0;i<listImages.length;i++ ){
+    listImages[i].addEventListener('mouseover',()=>{
+        
+    previewImage.src = listImages[i].src 
+    listImages[i].style.border = '3px solid black';
+    
+})
+listImages[i].addEventListener('mouseout',()=>{
+        
+  
+    listImages[i].style.border = '3px solid transparent';
+    
+})
+}
